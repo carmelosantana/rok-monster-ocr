b315a2ab51c91ee4ca396a34f169ada6f53425d0
Carmelo Santana <me@carmelosantana.com>
2020-10-13 16:06:46 -0400

    Fixed loading my-config.php
    Moved media path defines

--- rok.php
+++ rok.php
@@ -7,9 +7,17 @@ defined('ROK_PATH') or define('ROK_PATH', __DIR__);
 defined('ROK_PATH_APP') or define('ROK_PATH_APP', ROK_PATH . '/app');
 defined('ROK_PATH_IMAGES') or define('ROK_PATH_IMAGES', ROK_PATH . '/images');
 
-// app
+// configs
 require_once ROK_PATH . '/config.php';
-file_exists(require_once ROK_PATH . '/my-config.php') or require_once ROK_PATH . '/my-config.php';
+if( file_exists(ROK_PATH . '/my-config.php') )
+    require_once ROK_PATH . '/my-config.php';
+
+// Path for media and working tmp
+defined('ROK_PATH_INPUT') or define('ROK_PATH_INPUT', dirname(ROK_PATH) . '/input');
+defined('ROK_PATH_OUTPUT') or define('ROK_PATH_OUTPUT', dirname(ROK_PATH) . '/output');
+defined('ROK_PATH_TMP') or define('ROK_PATH_TMP', dirname(ROK_PATH) . '/tmp');
+
+// app
 require_once ROK_PATH_APP . '/app.php';
 require_once ROK_PATH_APP . '/cli.php';
 require_once ROK_PATH_APP . '/ffmpeg.php';
