5f0e19dd1d606ebca9ffd4a474d40ec97c6a06a1
Carmelo Santana <me@carmelosantana.com>
2020-11-05 10:26:11 -0500

    Added image resize for comparision

--- app/lib-images.php
+++ app/lib-images.php
@@ -14,7 +14,8 @@ use Treinetic\ImageArtist\lib\Text\Write\ImagickWritingStrategy;
  */
 // compare images
 // https://stackoverflow.com/questions/37581147/percentage-of-pixels-that-have-changed-in-an-image-in-php#37594159
-function image_compare_get_distortion($image_path_1=null, $image_path_2=null){
+// https://stackoverflow.com/questions/4684023/how-to-check-if-an-integer-is-within-a-range-of-numbers-in-php
+function image_compare_get_distortion($image_path_1=null, $image_path_2=null, $resize=true){
 	// check for errors
 	foreach ( [$image_path_1, $image_path_2] as $image )
 		if ( !$image or !file_exists($image) )
@@ -23,6 +24,15 @@ function image_compare_get_distortion($image_path_1=null, $image_path_2=null){
 	// load up
 	$image1 = new Imagick($image_path_1);
 	$image2 = new Imagick($image_path_2);
+
+	$w1 = $image1->getImageWidth();
+	$h1 = $image1->getImageHeight();
+	$h2 = $image2->getImageHeight();
+	$diff = 100;
+
+	if ( $resize and !(($h1-$diff <= $h2) and ($h2 <= $h1+$diff)) ){
+		$image2->scaleImage($w1, $h1);
+	}
 
 	// compare
 	$result = $image1->compareImages($image2,Imagick::METRIC_MEANABSOLUTEERROR);
