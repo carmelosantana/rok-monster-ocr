b5df274414c8dfaf9604bbf495f17ff98ad5bb4e
Carmelo Santana <me@carmelosantana.com>
2021-04-10 23:12:48 -0400

    Changed methods to camelCase
    Removed text_remove_non_numeric()

--- src/Transformer.php
+++ src/Transformer.php
@@ -8,19 +8,13 @@ use carmelosantana\RoKMonster\TinyCLI;
 
 class Transformer
 {
-    public static function str_remove_non_numeric(string $string): string
+    public static function strRemoveNonNumeric(string $string): string
     {
         return preg_replace('/[^0-9,.]+/', '', $string);
     }
 
-    public static function str_remove_extra_lines(string $string): string
+    public static function strRemoveExtraLineBreaks(string $string): string
     {
         return preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $string);
-    }
-
-    public static function text_remove_non_numeric(string $string): string
-    {
-        TinyCLI::cli_echo('This is deprecated: text_remove_non_numeric()');
-        return self::str_remove_non_numeric($string);
     }
 }
